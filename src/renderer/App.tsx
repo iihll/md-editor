4c61853fa84f62386882e43e216249ff33255980
iihll <iihll.dev@gmail.com>
2021-12-05 12:10:53 +0800

    Add codemirror

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,15 @@
-import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import { useState } from 'react';
 
-const Hello = () => {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="app">
+      <button type="button" onClick={() => setCount(count + 1)}>
+        count is: {count}
+      </button>
     </div>
   );
-};
-
-export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Hello} />
-      </Switch>
-    </Router>
-  );
 }
+
+export default App;
